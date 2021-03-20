1cf773b0c8160a33422ca1a1e8095c905aefdecd
tastaslim <taslimarif600@gmail.com>
2021-03-21 00:39:18 +0530

    DataBase Tutorial

--- what is databse.py
+++ what is databse.py
@@ -1,3 +1,14 @@
 print('What is DataBase?')
-print('A databse is collection of inter-related data which can be retrieved, updated and deleted efficiently.')
-print('Types of database:\n1. Relational database\n2. ')
+print('A databse is collection of inter-related data which can be created, retrieved, updated and deleted (CRUD operations) efficiently.')
+print('2 main types of databases:\n1. Relational database\n2. Non Relational database')
+print('Relational database:\n Organize data into one or more tables. Each table contains a row and a column.\n2. A unique key(Primary key) Identifies each row.')
+print('Non Relational databse:\n Organize data in the form of not table but in Graph, key-value pair, JSON, XML etc.')
+print('Example of non relational:\n')
+nonrelational_databse_data={
+    "taslim":"1",
+    "amir":"2",
+    "asad":"3",
+    "shamim":"4"
+}
+
+print(nonrelational_databse_data)
