e1195fc9a790cfa8c0a548bdf15bb9171acf9f4c
tastaslim <taslimarif600@gmail.com>
2021-03-20 12:15:19 +0530

    DataBase Tutorial

---
+++ what is databse.py
@@ -0,0 +1,3 @@
+print('What is DataBase?')
+print('A databse is collection of inter-related data which can be retrieved, updated and deleted efficiently.')
+print('Types of database:\n1. Relational database\n2. ')
